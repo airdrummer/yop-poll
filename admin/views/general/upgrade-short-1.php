e2fbba28f73f5d262d99f18952e168c9d47bedb3
tom wible <air_drummer@verizon.net>
2023-07-12 10:56:26 -0400

    v6.5.25

---
+++ admin/views/general/upgrade-short-1.php
@@ -0,0 +1,92 @@
+<div id="yop-poll-upgrade-modal-short" class="modal fade" role="dialog" style="margin-top: 10px;">
+    <div class="modal-dialog modal-lg">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header" style="border-bottom: 0px!important;">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body yop-poll-pro-section-content">
+                <div class="row">
+                    <div class="col-md-12 pro-section-text pro-section-pre-headline">
+						<i>
+							Don't struggle to make your polls stand out the hard way!
+						</i>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 pro-section-headline text-center">
+						"Elevate Your Polls with Our Exclusive Pro Upgrade!"
+                    </div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text pro-section-after-headline">
+						Upgrade to "YOP Poll Pro" version today and access a diverse range of incredible features that will revolutionize your audience engagement!
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 pro-section-text pro-section-below-headline">
+						Exciting news! With "YOP Poll Pro" you can now experience:
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 pro-section-text">
+						<ul>
+							<li>
+								Get Your Polls Noticed with 30+ Eye-Catching Designs!
+							</li>
+							<li class="yop-poll-text-bold">
+								Take Your Polls to the Next Level with Image-Driven Questions!
+							</li>
+							<li>
+								Engage Your Voters with Dynamic Text Slider Questions!
+							</li>
+							<li class="yop-poll-text-bold">
+								Seamlessly Integrate Google into Your Polls for More Accurate Results!
+							</li>
+							<li>
+								Create a Sense of Urgency and Boost Engagement with Our Countdown Timer!
+							</li>
+							<li class="yop-poll-text-bold">
+								Unleash the Power of Multiple Questions for Deeper Insights from Your Audience!
+							</li>
+							<li>
+								Spice Up Your Polls with Video-Based Questions to Capture Attention!
+							</li>
+							<li class="yop-poll-text-bold">
+								Connect and Build Relationships on Facebook Using Our Integration Feature for Polls!
+							</li>
+							<li>
+								Easily Analyze Results with Visually Stunning Pie Charts for Instant Feedback!
+							</li>
+							<li class="yop-poll-text-bold">
+								Encourage Candid Feedback and Honest Opinions with Our Anonymous Voting Option!
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text">
+						Our "YOP Poll Pro" has everything you need to create high-converting polls that deliver results.
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text pro-section-pre-footer-text">
+						So why wait?
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text sh-2-pro-section-footer-text">
+						<u>Upgrade now</u> and  witness how effortless it is to elevate your polls from good to great with our exclusive "YOP Poll Pro" features and benefits!
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text">
+						<a class="btn yop-poll-pro-button more-info-link" href="https://yop-poll.com/upgrade/?utm_source=wordpress&utm_campaign=yoplite&utm_medium=nshort1" target="_blank">
+							Yes! I want this now!
+						</a>
+					</div>
+				</div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ admin/views/general/upgrade-short-2.php
@@ -0,0 +1,92 @@
+<div id="yop-poll-upgrade-modal-short" class="modal fade" role="dialog" style="margin-top: 10px;">
+    <div class="modal-dialog modal-lg">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header" style="border-bottom: 0px!important;">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body yop-poll-pro-section-content">
+                <div class="row">
+                    <div class="col-md-12 pro-section-text pro-section-pre-headline">
+						<i>
+							Stop trying to make your polls stand out the hard way! ...
+						</i>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 pro-section-headline text-center">
+						Transform Your Polls with "YOP Poll Pro" Upgrade 
+                    </div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text pro-section-after-headline">
+						Upgrade to our "YOP Poll Pro" version today and unlock a wide range of incredible features that will help you <u><b>engage your audience</b></u> like never before!
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 pro-section-text pro-section-below-headline">
+						Finally! Now with "YOP Poll Pro" you can enjoy:
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 pro-section-text">
+						<ul>
+							<li>
+								Make Your Polls Pop with 30+ Stunning Designs!
+							</li>
+							<li class="yop-poll-text-bold">
+								Add a New Dimension to Your Polls with Image-Based Questions!
+							</li>
+							<li>
+								Let Your Voters Slide into Action with Text Slider Questions!
+							</li>
+							<li class="yop-poll-text-bold">
+								Supercharge Your Polls with Seamless Google Integration!
+							</li>
+							<li>
+								Create Urgency and Boost Participation with Our Countdown Timer Feature!
+							</li>
+							<li class="yop-poll-text-bold">
+								Get More Insights from Your Audience with Multiple Question Options!
+							</li>
+							<li>
+								Add Some Spark to Your Polls with Video-Based Questions!
+							</li>
+							<li class="yop-poll-text-bold">
+								Connect and Engage with Voters on Facebook Using Our Integration Feature!
+							</li>
+							<li>
+								Visualize Results Like Never Before with Pie Chart Analytics!
+							</li>
+							<li class="yop-poll-text-bold">
+								Keep Things Anonymous and Encourage Honest Feedback with Our Anonymous Voting Option!
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text">
+						With our "YOP Poll Pro", we've got everything you need to create high-converting polls that get results.
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text pro-section-pre-footer-text">
+						So why wait?
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text sh-2-pro-section-footer-text">
+						<u>Upgrade now</u> and discover how easy it is to take your polls from good to great with our exclusive "YOP Poll Pro" features!
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center pro-section-text">
+						<a class="btn yop-poll-pro-button more-info-link" href="https://yop-poll.com/upgrade/?utm_source=wordpress&utm_campaign=yoplite&utm_medium=nshort2" target="_blank">
+							Yes! I want this now!
+						</a>
+					</div>
+				</div>
+            </div>
+        </div>
+    </div>
+</div>
